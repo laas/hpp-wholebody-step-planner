ba916da46f453245ae9b95e7049b6b968d59faa3
Florent Lamiraux <florent@laas.fr>
2011-07-03 14:35:26 +0200

    Add a header for forward declarations.

---
+++ include/hpp/wholebody-step-planner/fwd.hh
@@ -0,0 +1,30 @@
+///
+/// Copyright (c) 2011 CNRS
+/// Authors: Florent Lamiraux
+///
+///
+// This file is part of hpp-wholebody-step-planner.
+// hpp-wholebody-step-planner is free software: you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
+//
+// hpp-wholebody-step-planner is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Lesser Public License for more details.  You should have
+// received a copy of the GNU Lesser General Public License along with
+// hpp-wholebody-step-planner.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+#ifndef HPP_WHOLEBODY_STEP_PLANNER_FWD_HH
+# define HPP_WHOLEBODY_STEP_PLANNER_FWD_HH
+
+namespace hpp {
+  namespace wholeBodyStepPlanner {
+    class Planner;
+    class PathOptimizer;
+    class wholeBodyConstraint;
+  } // wholeBodyStepPlanner
+} // namespace hpp
+#endif // HPP_WHOLEBODY_STEP_PLANNER_FWD_HH
