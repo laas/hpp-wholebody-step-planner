96a89106cf849c769a9f897c5340923160b3a618
Florent Lamiraux <florent@laas.fr>
2011-12-29 21:28:14 +0100

    Notify LGPL license.

--- include/hpp/wholebody-step-planner/position-motion-constraint.hh
+++ include/hpp/wholebody-step-planner/position-motion-constraint.hh
@@ -1,5 +1,21 @@
-// Copyright (C) 2011 by Sebastien Dalibard.
+// Copyright (C) 2011,2012 CNRS-LAAS
+// Author: Sebastien Dalibard.
 //
+// This file is part of the hpp-wholebody-step-planner.
+//
+// hpp-wholebody-step-planner is free software: you can redistribute
+// it and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+//
+// hpp-wholebody-step-planner is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with hpp-wholebody-step-planner.  If not, see
+// <http://www.gnu.org/licenses/>.
 
 #ifndef CHPP_GIK_POSITION_MOTION_CONSTRAINT
 #define CHPP_GIK_POSITION_MOTION_CONSTRAINT
